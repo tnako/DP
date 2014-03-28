5116f9de6538e38f50754bb6081863efa3adf5f9
C.H.A.D.o <chado@chado.by>
2014-03-28 21:18:58 +0400

    Слишком много czmq, надо будет переделать на чистом zmq

---
+++ my/worker.c
@@ -0,0 +1,18 @@
+#include "mdwrkapi.c"
+
+int main (int argc, char *argv [])
+{
+    mdwrk_t *session = mdwrk_new("tcp://localhost:5555", "echo", 1);
+
+    zmsg_t *reply = NULL;
+    while (true) {
+        zmsg_t *request = mdwrk_recv (session, &reply);
+        if (request == NULL) {
+            break;              //  Worker was interrupted
+		}
+		
+        reply = request;        //  Echo is complex... :-)
+    }
+    mdwrk_destroy(&session);
+    return 0;
+}
