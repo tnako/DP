e65798e79ceb6a218220d64400bc1c1d4005d780
Коршиков Антон <chado@bingo-boom.ru>
2014-04-08 17:31:50 +0400

    Timer in object

--- my/majordomo/src/broker.c
+++ my/majordomo/src/broker.c
@@ -1,40 +1,26 @@
 #include "broker.h"
 
 #include <sys/timerfd.h>
+#include <sys/signalfd.h>
+#include <signal.h>
 
+#include <unistd.h>
 
-int timerfd;
-
-static void func()
+static void func(pobj_loop* loop, const puint32 epoll_events)
 {
-    plog_warn(".......");
+    plog_warn("....... %x | %u", loop, epoll_events);
+    pobj_internal_timer_stop(loop);
 }
 
 void broker_main_loop()
 {
-    struct itimerspec new_value;
-    struct timespec now;
 
-    if (clock_gettime(CLOCK_REALTIME, &now) == -1) {
-                 perror("clock_gettime");
-    }
-
-    new_value.it_value.tv_sec = now.tv_sec + 1;
-    new_value.it_value.tv_nsec = now.tv_nsec;
-    new_value.it_interval.tv_sec = 0;
-
-    int timerfd = timerfd_create(CLOCK_REALTIME, 0);
-    if (timerfd) {
-        perror("timerfd_create:");
-    }
-    int res = timerfd_settime(timerfd, TFD_TIMER_ABSTIME, &new_value, 0);
-    if(res < 0) {
-        perror("timerfd_settime:");
-    }
+    struct timespec ts = { .tv_sec = 1, .tv_nsec = 0 };
 
     pobj_loop *looper = pobj_create(128, false);
-    pobj_register_event(looper, func, timerfd, EPOLLIN);
-    plog_warn("!!!!!");
+    pobj_internal_timer_start(looper, 1, ts, func);
 
     pobj_run(looper);
+
+    pobj_destroy(&looper);
 }
